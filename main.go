e5408f21ac842972643537abc3a90191d1dc2ae4
Wes C <wes.clyburn@proton.me>
2026-08-09 22:49:54 -0500

    refactoring 1

--- main.go
+++ main.go
@@ -14,30 +14,53 @@ import (
 var cfg = configuration.GetConfiguration()
 
 func main() {
-	serverShutdown := make(chan struct{})
+	svr, serverShutdown := startServer()
+	defer func(svr *server.Server) {
+		err := svr.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}(svr)
+	<-waitForShutdownSignal()
+	log.Println("Shutdown signal received")
+	stopServer(svr)
+	<-serverShutdown
+	log.Println("Exiting normally.")
+
+}
+
+func startServer() (*server.Server, chan struct{}) {
 	log.Println("Starting server...")
-	var svr = server.Server{
+	serverShutdown := make(chan struct{})
+	var svr = &server.Server{
 		Addr: cfg.HostBindIp + ":" + cfg.Port,
 	}
 	go func() {
 		if err := svr.ListenAndServe(cfg.GopherRoot); err != nil {
 			log.Fatal(err)
 		}
-		serverShutdown <- struct{}{}
+		close(serverShutdown)
 	}()
+
+	return svr, serverShutdown
+}
+
+func waitForShutdownSignal() chan os.Signal {
 	// Capture Ctrl‑C and SIGTERM
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-	<-sig
-	log.Println("Shutdown signal received")
+	return sig
+}
+
+func stopServer(svr *server.Server) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+	log.Println("Shutting down server...")
 	var err = svr.Shutdown(ctx)
 	if err != nil {
 		log.Println(err)
+		return
 	}
-	log.Println("Shutdown complete")
+	log.Println("Shutdown complete.")
 
-	<-serverShutdown
-	log.Println("Done.")
 }
